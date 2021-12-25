91dee2bb62b8459a1e83f029f6943c8fde02177c
PAULO HENRIQUE DA SILVA NETO <86234861+PauloHSNeto@users.noreply.github.com>
2021-12-25 16:52:11 -0300

    adicionando commentarios

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ dex= requests.get('https://www.serebii.net/pokedex/fire.shtml')
 #print(dex.text)
 soup = BeautifulSoup(dex.text, "lxml")#.text necessario
 infos = soup.find_all(class_="fooinfo")
-charizard = infos.find("")
+textos_li= soup.find_all("li")
+
 for i in infos:
-    print(i.text)  #mostra todos os pokemon de fogo da gen 1 com os stats
+    print(i.text.replace("\n",""))  #mostra todos os pokemon de fogo da gen 1 com os stats sem os \n
